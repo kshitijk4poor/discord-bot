2f5915b2c28138ba19fca39e4271489f29eb633b
Kshitij <kshitijkapoor0611@gmail.com>
2023-04-27 21:13:54 +0530

    gpt bot init

---
+++ index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const channelID = '1100170161694183427';
+const targetChannelID = '1101092357635579944';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  setInterval(runFunction, 1);
+});
+
+async function runFunction() {
+  const channel = await client.channels.fetch(channelID);
+
+  const members = channel.members.filter(
+    member => !member.user.bot && member.lastMessage.channel.id === channelID
+  );
+
+  if (members.size > 0) {
+    const randomMember = members.random();
+    const targetChannel = await client.channels.fetch(targetChannelID);
+    await targetChannel.send(`Congratulations to <@${randomMember.id}> for being selected randomly!`);
+  }
+}
+client.login(process.env.BOT_TOKEN);
+console.log('bot is online');
